4e53ce6d6fb9ec198a7ff821b8fecea02692d27a
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-07-04 17:15:07 +0530

    added react app

---
+++ client_s/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shubham</h1>
+      <h1>Shubham</h1> <h1>Shubham</h1>
+      <h1>Shubham</h1>
+    </div>
+  );
+}
+
+export default App;
